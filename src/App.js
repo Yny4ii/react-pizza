044061f3ae539fe0be4b825fd5d5ea8881205b33
Yny4ii <ilya.semyonov.03@mail.ru>
2022-05-29 13:09:07 +0300

    Добавлен рендер данных из БД

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import './scss/app.scss'
+import Header from "./components/header/Header";
+import Categories from "./components/categories/Categories";
+import Sort from "./components/sort/Sort";
+import PizzaBlock from "./components/pizzaBlock/PizzaBlock";
+import pizzas from './assets/db.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {pizzas.map((pizza) => (
+                            <PizzaBlock
+                                pizzaName={pizza.name}
+                                pizzaPrice={pizza.price}
+                                pizzaImageUrl={pizza.imageUrl}
+                                pizzaTypes={pizza.types}
+                                pizzaSizes={pizza.sizes}
+                                pizzaCategory={pizza.category}
+                                pizzaRating={pizza.rating}
+                            />
+
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
 
 export default App;
